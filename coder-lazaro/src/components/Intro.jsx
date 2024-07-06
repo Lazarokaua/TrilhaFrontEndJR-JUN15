2850f75ad0c7ecc1cc4cf5ed8f7b5ec2026118e4
Lazarokaua <lazarokaua4@gmail.com>
2024-07-05 21:03:34 -0300

    intro part left done, photo profile

--- coder-lazaro/src/components/Intro.jsx
+++ coder-lazaro/src/components/Intro.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-import "../input.css"
-import profile from "../assets/profile.jpg"
-import gitHub from "../assets/github.svg"
-import linkedIn from "../assets/linkedin.svg"
-import email from "../assets/email.svg"
+import "../input.css";
+import profile from "../assets/profile.jpg";
+
+import {GitHubIcon, LinkedInIcon, EmailIcon} from "../components/Icones"
+
 
 export function Intro() {
   return (
-    <section className="">
-    <div>
-      <p>👋 Saudações!</p>
-      <h1>Lázaro Kauã</h1>
-      <p>Front-end developer · BI analyst</p>
-    </div>
+    <section className="flex justify-around">
+      <div>
+        <p>👋 Saudações!</p>
+        <h1>Lázaro Kauã</h1>
+        <p>Front-end developer · BI analyst</p>
+      </div>
 
-    <div>
-      <img src={profile} alt="Lázaro programador" className="w-96 h-96"/>
-      <img src={gitHub} alt="icone github"/>
-      <img src={linkedIn} alt="icone linkedIn"/> 
-      <img src={email} alt="icone email"/>
-      {/* mudar os icones para a cor preta */}
-    </div>
+      <div className="flex flex-col">
+        <img src={profile} alt="Lázaro programador" className="w-96 h-96 rounded-full mt-16 border-2 border-secundary-0 shadow-inner" />
+        <div className="flex justify-center gap-8 mt-4 mr-2">
+          <a href="/">
+            <GitHubIcon />
+          </a>
+          <a href="/">
+            <LinkedInIcon />
+          </a>
+          <a href="/">
+            <EmailIcon />
+          </a>
+        </div>
+        {/* mudar os icones para a cor preta */}
+      </div>
     </section>
   );
 }
--- coder-lazaro/src/components/NavBar.jsx
+++ coder-lazaro/src/components/NavBar.jsx
@@ -4,14 +4,19 @@ import logo from "../assets/logo_coder-white.webp";
 export function NavBar() {
   return (
     <header className="bg-back-0 flex justify-between">
-      <img src={logo} alt="logo do codigo certo" className="ml-6 mt-2"/>
-
+      <a href="/">
+        <img src={logo} alt="logo do codigo certo" className="ml-6 mt-2" />
+      </a>
       <ul className="flex mt-12 gap-12 mr-12">
         <a href="/">
-          <li className="font-raleway font-bold text-xl text-detail-0 hover:text-secundary-0">inicial</li>
+          <li className="font-raleway font-bold text-xl text-detail-0 hover:text-secundary-0">
+            inicial
+          </li>
         </a>
         <a href="/">
-          <li className="font-raleway font-bold text-xl text-detail-0 hover:text-secundary-0">Sobre mim</li>
+          <li className="font-raleway font-bold text-xl text-detail-0 hover:text-secundary-0">
+            Sobre mim
+          </li>
         </a>
         <a href="/">
           <li className="font-raleway font-bold text-xl text-detail-0 hover:text-secundary-0">
@@ -20,10 +25,14 @@ export function NavBar() {
           </li>
         </a>
         <a href="/">
-          <li className="font-raleway font-bold text-xl text-detail-0 hover:text-secundary-0">Projetos</li>
+          <li className="font-raleway font-bold text-xl text-detail-0 hover:text-secundary-0">
+            Projetos
+          </li>
         </a>
         <a href="/">
-          <li className="font-raleway font-bold text-xl text-detail-0 hover:text-secundary-0">Tecnologias</li>
+          <li className="font-raleway font-bold text-xl text-detail-0 hover:text-secundary-0">
+            Tecnologias
+          </li>
         </a>
       </ul>
     </header>
